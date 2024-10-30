266f0a1350f5d46ca9d481c82295d3a6e71b787c
exslym <76844562+exslym@users.noreply.github.com>
2024-10-30 20:39:24 +0100

    added Hero section with search form

---
+++ components/SearchForm.tsx
@@ -0,0 +1,25 @@
+import { Search } from 'lucide-react';
+import Form from 'next/form';
+import React from 'react';
+import SearchFormReset from './SearchFormReset';
+
+const SearchForm = ({ query }: { query?: string }) => {
+	return (
+		<Form action='/' scroll={false} className='search-form'>
+			<input
+				name='query'
+				defaultValue={query}
+				placeholder='Search Startups'
+				className='search-input'
+			/>
+			<div className='flex gap-2'>
+				{query && <SearchFormReset />}
+				<button type='submit' className='search-btn text-white'>
+					<Search className='size-6' />
+				</button>
+			</div>
+		</Form>
+	);
+};
+
+export default SearchForm;
